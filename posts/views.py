e3d5c7c6a1b7da80fc965efaead4f67af08ab3e3
cazoni007 <sebastiancazon301@gmail.com>
2023-04-08 20:12:01 -0400

    usando bootstrap y leyendo los posts del diccionario de datos para cargarlos a la pagina, feed.html

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,5 @@
 #django
 from django.shortcuts import render
-from django.http import HttpResponse
 #Utilities
 from datetime import datetime
 
@@ -11,33 +10,27 @@ posts = [
         'username': 'Johnny Silverhand',
         'age': '27',
         'timestamp': datetime.now().strftime('%A %d de %B de %Y. Hora: %H:%M:%S%p'),
-        'img': 'https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/hc_1440x810/public/media/image/2021/12/cyberpunk-2077-johnny-silverhand-keanu-reeves-2567861.jpg?itok=1zhR0CFK'
+        'profilePicture': 'https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/hc_1440x810/public/media/image/2021/12/cyberpunk-2077-johnny-silverhand-keanu-reeves-2567861.jpg?itok=1zhR0CFK',
+        'imgPost': 'https://cloudfront-us-east-1.images.arcpublishing.com/infobae/3MSH4NDQ6BFDXBQ3BLURL354DI.jpg',
     },
     {
         'name': 'V',
         'username': 'cazoni',
         'age': '25',
         'timestamp': datetime.now().strftime('%A %d de %B de %Y. Hora: %H:%M:%S%p'),
-        'img': 'https://alfabetajuega.com/hero/2020/07/Cyberpunk-2077.jpg?width=1200&aspect_ratio=16:9'
+        'profilePicture': 'https://alfabetajuega.com/hero/2020/07/Cyberpunk-2077.jpg?width=1200&aspect_ratio=16:9',
+        'imgPost': 'https://i.blogs.es/024bf8/cyberpunk2077_lucky_number_13_rgb-en/1366_2000.jpeg',
     },
     {
         'name': 'Kirito',
         'username': 'El espadachin puerco',
         'age': '17',
         'timestamp': datetime.now().strftime('%A %d de %B de %Y. Hora: %H:%M:%S%p'),
-        'img': 'https://static.wikia.nocookie.net/sao/images/5/56/Doble_empuñadura_Kirito.png/revision/latest?cb=20130107144520&path-prefix=es'
+        'profilePicture': 'https://static.wikia.nocookie.net/sao/images/5/56/Doble_empuñadura_Kirito.png/revision/latest?cb=20130107144520&path-prefix=es',
+        'imgPost': 'https://i.ytimg.com/vi/GwGd3teG6gg/maxresdefault.jpg',
     }
 ]
 # Create your views here.
 
 def post(request):
-    """Creamos una lista que almacenara los posts"""
-    contenido = []
-    """Un For el cual añadira la lista de posts que creamos arriba a la lista de contenido, y ademas el tipo de letra. Con html"""
-    for post in posts:
-        contenido.append("""
-            <p><strong>{name} - <i>{username}</i></strong></p>
-            <p><small>{age} - <i>{timestamp}</i></small></p>
-            <figure><img src="{img}"/></figure>
-        """.format(**post))
-    return HttpResponse('<br>'.join(contenido))
+    return render(request, 'feed.html', {'posts': posts})
